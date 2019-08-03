88ab690070ab300cba88ace5d2839c38f8aa03e4
ysk <y.sawa4787@gmail.com>
2019-08-03 23:05:52 +0900

    アプリケーション全体でconfig値を書き換える

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,5 +24,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        config([
+            'sample.data' => ['いち', 'にー', 'さん']
+        ]);
     }
 }
