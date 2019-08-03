f3a891f66f3b225d42adbfbbff619797d6bd0cbf
ysk <y.sawa4787@gmail.com>
2019-08-03 19:56:08 +0900

    404のレイアウト変更

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,7 @@
+@extends('errors::minimal')
+{{-- @extends('errors::illustrated-layout') --}}
+{{-- @extends('errors::layout') --}}
+
+@section('title', __('Not Found'))
+@section('code', '404')
+@section('message', __('Not Found'))
